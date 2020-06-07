acc79dd94348778cf73dec5663880a154e25f5ed
darinamazur <allottcentr@gmail.com>
2020-06-07 16:56:58 +0300

    finish testing

--- gtests/main.cpp
+++ gtests/main.cpp
@@ -3,21 +3,35 @@
 #include "group.hpp"
 #include "song.hpp"
 
-TEST(test001,test){
+TEST(testCheck,testFunc){
     int a = 5, b = 8;
     int actual = sum(a,b);
     int expected = 13;
     ASSERT_EQ(expected,actual);
 }
 
-TEST(test002,test){
-    int a = 5, b = 8;
-    int actual = sum(a,b);
-    int expected = 22;
-    ASSERT_EQ(expected,actual);
+TEST(testProcessSong,test1){
+    Song s1,s2;
+    s1.stars=2;
+    Group g;
+    Song s3 = g.processSong(s2);
+    ASSERT_EQ(s1.stars,s3.stars);
 }
-
-TEST(test003,test){
+TEST(testProcessSong,test2){
+    Song s1,s2;
+    s1.stars=2;
+    Group g;
+    Song s3 = g.processSong(s2);
+    ASSERT_EQ(s1.stars,s3.stars);
+}
+TEST(testProcessSong,test3){
+    Song s1,s2;
+    s1.stars=2;
+    Group g;
+    Song s3 = g.processSong(s2);
+    ASSERT_EQ(s1.stars,s3.stars);
+}
+TEST(testProcessSong,test4){
     Song s1,s2;
     s1.stars=2;
     Group g;
